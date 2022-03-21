ae09cb99cc6d9c19ae78849c43c50b834edccb8d
afify <hassan@afify.dev>
2022-03-21 11:58:08 +0300

    openbsd syscalls

--- syscalls.s
+++ syscalls.s
@@ -6,8 +6,12 @@
 %ifdef Linux
 	%define SYS_exit 60
 	%define SYS_write 1
-	%define SYS_gettimeofday 96
 	%define SYS_execve 59
+%elifdef OpenBSD
+	%define SYS_exit 1
+	%define SYS_write 4
+	%define SYS_execve 59
+	%define SYS_pledge 108
 %else
 	%fatal "OS not supported"
 %endif
